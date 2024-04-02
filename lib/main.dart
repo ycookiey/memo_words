f8bb94fa8fc392384a9fad2714f0f464b5bbcfaf
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-04-02 12:02:31 +0900

    add:providerScopeを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memo_words/test.dart';
 
-
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
