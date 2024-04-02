861de0f2fe40183e6d49ca5ed74cef5ad932c364
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-04-02 12:10:27 +0900

    add:アプリをfirebaseに接続、初期化 補足:firebase系のapiは"just identifies your Firebase project on the Google servers. It is not a security risk for someone to know it."らしくコミットOK

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memo_words/test.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: MyApp()));
 }
 
