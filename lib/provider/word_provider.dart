4c966f4ab65b06a5947a92c94b2829ea16a62512
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-04-03 23:24:42 +0900

    update:Provider->StateNotifierProviderに変更

--- lib/provider/word_provider.dart
+++ lib/provider/word_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memo_words/model/firestore/firestore_model.dart';
 import 'package:memo_words/repository/word_repository.dart';
 import 'package:memo_words/view_model/word_view_model.dart';
 
-final wordViewModelProvider = Provider<WordViewModel>((ref) {
+final wordViewModelProvider =
+    StateNotifierProvider<WordViewModel, List<Word>>((ref) {
   return WordViewModel(WordRepository());
 });
--- lib/view_model/word_view_model.dart
+++ lib/view_model/word_view_model.dart
@@ -1,16 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memo_words/model/firestore/firestore_model.dart';
 import 'package:memo_words/repository/word_repository.dart';
 
-class WordViewModel {
+class WordViewModel extends StateNotifier<List<Word>> {
   final WordRepository _wordRepository;
 
-  WordViewModel(this._wordRepository);
-
-  Future<Word?> addWord(String word, String meaning) async {
-    return _wordRepository.addWord(word, meaning);
+  WordViewModel(this._wordRepository) : super([]) {
+    initialize();
   }
 
-  Future<List<Word>> getWords() async {
-    return _wordRepository.getWords();
+  Future<void> initialize() async {
+    var words = await _wordRepository.getWords();
+    state = words;
+  }
+
+  Future<void> addWord(String word, String meaning) async {
+    final newWord = await _wordRepository.addWord(word, meaning);
+    if (newWord != null) {
+      state = [...state, newWord];
+    }
+  }
+
+  Future<void> getWords() async {
+    List<Word> words = await _wordRepository.getWords();
+    state = words;
   }
 }
