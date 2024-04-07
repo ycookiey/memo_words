140c025aed82bc0a0b4b6e8900ea0a6b4fbda7b8
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-04-07 17:58:26 +0900

    add:ログイン画面の追加

---
+++ lib/view/login_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memo_words/provider/auth_view_model_provider.dart';
+import 'package:memo_words/view/home_page.dart';
+
+class LoginPage extends ConsumerWidget {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ログイン')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'メールアドレス'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'パスワード'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final email = _emailController.text;
+                final password = _passwordController.text;
+
+                ref
+                    .read(authViewModelProvider.notifier)
+                    .loginOrCreateAccount(email, password)
+                    .then((_) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                }).catchError((error) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('エラー'),
+                      content: Text(error.toString()),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    ),
+                  );
+                });
+              },
+              child: const Text('ログイン / 新規登録'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
