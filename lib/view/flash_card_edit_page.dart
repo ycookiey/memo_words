da82ed615f79aca8c38c9397d0540e357a8c6671
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-08-29 14:46:42 +0900

    update:単語帳編集ページを作成ページと同じデザインに更新

--- lib/view/flash_card_edit_page.dart
+++ lib/view/flash_card_edit_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:memo_words/model/firestore/flashcard_model.dart';
-import 'package:memo_words/model/firestore/word_model.dart';
 import 'package:memo_words/provider/word_provider.dart';
 
 class FlashCardEditPage extends ConsumerStatefulWidget {
@@ -12,10 +10,11 @@ class FlashCardEditPage extends ConsumerStatefulWidget {
 }
 
 class _FlashCardEditState extends ConsumerState<FlashCardEditPage> {
-  final wordController = TextEditingController();
-  final meaningController = TextEditingController();
   final flashcardNameController = TextEditingController();
+  final _flashCardInputFormKey = GlobalKey<FormState>();
+  final _wordInputFormKey = GlobalKey<FormState>();
   String? selectedFlashcardId;
+  List<WordPair> wordPairs = [];
 
   @override
   void initState() {
@@ -27,8 +26,18 @@ class _FlashCardEditState extends ConsumerState<FlashCardEditPage> {
           selectedFlashcardId = selectedId;
         });
         _updateFlashcardName();
+        _loadWords();
       }
     });
+  }
+
+  @override
+  void dispose() {
+    flashcardNameController.dispose();
+    for (var pair in wordPairs) {
+      pair.dispose();
+    }
+    super.dispose();
   }
 
   void _updateFlashcardName() {
@@ -42,189 +51,224 @@ class _FlashCardEditState extends ConsumerState<FlashCardEditPage> {
     }
   }
 
+  void _loadWords() {
+    if (selectedFlashcardId != null) {
+      final words = ref
+          .read(wordViewModelProvider.notifier)
+          .getWordsForFlashcard(selectedFlashcardId!);
+      setState(() {
+        wordPairs = words
+            .map((word) => WordPair(
+                  wordController: TextEditingController(text: word.word),
+                  meaningController: TextEditingController(text: word.meaning),
+                  id: word.id,
+                ))
+            .toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final flashcards = ref.watch(wordViewModelProvider);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('単語帳編集'),
       ),
-      body: Column(
-        children: [
-          _buildFlashcardSelector(flashcards),
-          _buildEditFlashcardSection(),
-          if (selectedFlashcardId != null) ...[
-            _buildAddWordSection(),
-            _buildWordList(selectedFlashcardId!),
-          ],
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateFlashcard,
+        child: const Icon(Icons.check),
+      ),
+      body: Form(
+        key: _flashCardInputFormKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: flashcardNameController,
+                  decoration: const InputDecoration(labelText: '単語帳名'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '単語帳名を入力してください';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                if (selectedFlashcardId != null) ...[
+                  Form(key: _wordInputFormKey, child: _buildWordInputList()),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _createNewWordPair,
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 
-  Widget _buildFlashcardSelector(List<Flashcard> flashcards) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: DropdownButton<String>(
-        value: selectedFlashcardId,
-        hint: const Text('単語帳を選択'),
-        isExpanded: true,
-        items: flashcards.map((flashcard) {
-          return DropdownMenuItem<String>(
-            value: flashcard.id,
-            child: Text(flashcard.name),
-          );
-        }).toList(),
-        onChanged: (String? newValue) {
-          setState(() {
-            selectedFlashcardId = newValue;
-          });
-          _updateFlashcardName();
-        },
-      ),
+  Widget _buildWordInputList() {
+    return Column(
+      children:
+          wordPairs.map((wordPair) => _buildWordInputRow(wordPair)).toList(),
     );
   }
 
-  Widget _buildEditFlashcardSection() {
+  Widget _buildWordInputRow(WordPair wordPair) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(vertical: 8),
       child: Row(
         children: [
           Expanded(
-            child: TextField(
-              controller: flashcardNameController,
-              decoration: const InputDecoration(labelText: '単語帳名'),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              final name = flashcardNameController.text;
-              if (name.isNotEmpty && selectedFlashcardId != null) {
-                ref
-                    .read(wordViewModelProvider.notifier)
-                    .updateFlashcard(selectedFlashcardId!, name);
-              }
-            },
-            child: const Text('単語帳名を更新'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildAddWordSection() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: wordController,
-            decoration: const InputDecoration(labelText: '単語'),
-          ),
-          TextField(
-            controller: meaningController,
-            decoration: const InputDecoration(labelText: '意味'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              final word = wordController.text;
-              final meaning = meaningController.text;
-              if (word.isNotEmpty &&
-                  meaning.isNotEmpty &&
-                  selectedFlashcardId != null) {
-                ref
-                    .read(wordViewModelProvider.notifier)
-                    .addWord(selectedFlashcardId!, word, meaning);
-                wordController.clear();
-                meaningController.clear();
-              }
-            },
-            child: const Text('単語を追加'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildWordList(String flashcardId) {
-    final words = ref
-        .watch(wordViewModelProvider.notifier)
-        .getWordsForFlashcard(flashcardId);
-
-    return Expanded(
-      child: ListView.builder(
-        itemCount: words.length,
-        itemBuilder: (context, index) {
-          final word = words[index];
-          return ListTile(
-            title: Text(word.word),
-            subtitle: Text(word.meaning),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () => _showEditDialog(flashcardId, word),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    ref
-                        .read(wordViewModelProvider.notifier)
-                        .deleteWord(flashcardId, word.id);
-                  },
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  void _showEditDialog(String flashcardId, Word word) {
-    final editWordController = TextEditingController(text: word.word);
-    final editMeaningController = TextEditingController(text: word.meaning);
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('単語を編集'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: editWordController,
-                decoration: const InputDecoration(labelText: '単語'),
-              ),
-              TextField(
-                controller: editMeaningController,
-                decoration: const InputDecoration(labelText: '意味'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('キャンセル'),
-            ),
-            TextButton(
-              onPressed: () {
-                ref.read(wordViewModelProvider.notifier).updateWord(
-                      flashcardId,
-                      word.id,
-                      editWordController.text,
-                      editMeaningController.text,
-                    );
-                Navigator.pop(context);
+            child: TextFormField(
+              focusNode: wordPair.wordFocusNode,
+              textInputAction: TextInputAction.next,
+              controller: wordPair.wordController,
+              decoration: const InputDecoration(
+                  labelText: '単語', labelStyle: TextStyle(fontSize: 12)),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '単語を入力してください';
+                }
+                return null;
               },
-              child: const Text('保存'),
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(wordPair.meaningFocusNode);
+              },
             ),
-          ],
-        );
-      },
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: TextFormField(
+              focusNode: wordPair.meaningFocusNode,
+              textInputAction: TextInputAction.next,
+              controller: wordPair.meaningController,
+              decoration: const InputDecoration(
+                  labelText: '意味', labelStyle: TextStyle(fontSize: 12)),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '意味を入力してください';
+                }
+                return null;
+              },
+              onFieldSubmitted: (_) => _focusNextField(wordPair),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.remove),
+            onPressed: () => _removeWordPair(wordPair),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _focusNextField(WordPair currentPair) {
+    int currentIndex = wordPairs.indexOf(currentPair);
+    if (currentIndex == wordPairs.length - 1) {
+      _createNewWordPair();
+    } else {
+      FocusScope.of(context)
+          .requestFocus(wordPairs[currentIndex + 1].wordFocusNode);
+    }
+  }
+
+  void _createNewWordPair() {
+    if (_wordInputFormKey.currentState!.validate()) {
+      setState(() {
+        wordPairs.add(WordPair());
+      });
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        FocusScope.of(context).requestFocus(wordPairs.last.wordFocusNode);
+      });
+    } else {
+      _focusFirstErrorField();
+    }
+  }
+
+  void _focusFirstErrorField() {
+    for (var wordPair in wordPairs) {
+      if (wordPair.wordController.text.isEmpty) {
+        FocusScope.of(context).requestFocus(wordPair.wordFocusNode);
+        return;
+      }
+      if (wordPair.meaningController.text.isEmpty) {
+        FocusScope.of(context).requestFocus(wordPair.meaningFocusNode);
+        return;
+      }
+    }
+  }
+
+  void _removeWordPair(WordPair pair) {
+    setState(() {
+      wordPairs.remove(pair);
+    });
+    if (pair.id != null) {
+      ref
+          .read(wordViewModelProvider.notifier)
+          .deleteWord(selectedFlashcardId!, pair.id!);
+    }
+  }
+
+  void _updateFlashcard() async {
+    if (_flashCardInputFormKey.currentState!.validate() &&
+        _wordInputFormKey.currentState!.validate()) {
+      final name = flashcardNameController.text;
+
+      try {
+        await ref
+            .read(wordViewModelProvider.notifier)
+            .updateFlashcard(selectedFlashcardId!, name);
+
+        for (var wordPair in wordPairs) {
+          if (wordPair.id != null) {
+            await ref.read(wordViewModelProvider.notifier).updateWord(
+                  selectedFlashcardId!,
+                  wordPair.id!,
+                  wordPair.wordController.text,
+                  wordPair.meaningController.text,
+                );
+          } else {
+            await ref.read(wordViewModelProvider.notifier).addWord(
+                  selectedFlashcardId!,
+                  wordPair.wordController.text,
+                  wordPair.meaningController.text,
+                );
+          }
+        }
+        Navigator.of(context).pop();
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('エラーが発生しました: $e')),
+        );
+      }
+    }
+  }
+}
+
+class WordPair {
+  final TextEditingController wordController;
+  final TextEditingController meaningController;
+  final FocusNode wordFocusNode = FocusNode();
+  final FocusNode meaningFocusNode = FocusNode();
+  final String? id;
+
+  WordPair({
+    TextEditingController? wordController,
+    TextEditingController? meaningController,
+    this.id,
+  })  : wordController = wordController ?? TextEditingController(),
+        meaningController = meaningController ?? TextEditingController();
+
+  void dispose() {
+    wordController.dispose();
+    meaningController.dispose();
+    wordFocusNode.dispose();
+    meaningFocusNode.dispose();
   }
 }
